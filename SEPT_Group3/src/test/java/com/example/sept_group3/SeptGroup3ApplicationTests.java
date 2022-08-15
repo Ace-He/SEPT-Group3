d6d1967a60e0434dc5e1e6745e61a2ed857b1c4f
jontygrbac <83052885+jontygrbac@users.noreply.github.com>
2022-08-15 20:58:03 +1000

    Add files via upload

---
+++ SEPT_Group3/src/test/java/com/example/sept_group3/SeptGroup3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sept_group3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeptGroup3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
