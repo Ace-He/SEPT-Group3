5a9975518e2f14c1b226caaca3703845c8b4f791
Nicholas Santosa <s3844591@student.rmit.edu.au>
2022-08-24 21:12:39 +1000

    Added login.dart

---
+++ SEPT_Group3/src/main/java/com/example/sept_group3/login.dart
@@ -0,0 +1,19 @@
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth < 600) {
+            return const LoginMobile();
+          } else if (constraints.maxWidth > 600 && constraints.maxWidth < 900) {
+            return const LoginTablet();
+          } else {
+            return const LoginDesktop();
+          }
+        },
+      ),
+    );
+  }
+}
