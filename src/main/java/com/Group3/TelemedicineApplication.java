acb399c8d0df05713d4e37cfc711dbee92499218
jontygrbac <s3659145@student.rmit.edu.au>
2022-08-25 16:07:41 +1000

    Connected RDS AWS to back-end

--- src/main/java/com/Group3/TelemedicineApplication.java
+++ src/main/java/com/Group3/TelemedicineApplication.java
@@ -1,13 +1,23 @@
 package com.Group3;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TelemedicineApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(TelemedicineApplication.class, args);
     }
 
+    @Bean
+    ApplicationRunner applicationRunner(PatientRepository patientRepository){
+        return args -> {
+            patientRepository.save(new Patient("jonty@example.com", "password123"));
+            patientRepository.save(new Patient("tony@example.com", "password321"));
+        };
+    }
 }
