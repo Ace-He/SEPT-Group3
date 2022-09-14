e590d72830aedfe0ab9421b54dd3a4713c42770b
jontygrbac <s3659145@student.rmit.edu.au>
2022-09-14 14:35:22 +1000

    Can receive formdata, now attempting to sent formdata to backend

--- patient_update_details/lib/Screens/Profile.dart
+++ patient_update_details/lib/Screens/Profile.dart
@@ -9,13 +9,74 @@ class profile extends StatefulWidget{
 }
 
 class profileState extends State<profile>{
+
+  void submit(){
+    print(firstController.text);
+    print(secondController.text);
+}
+
+  final padding = 5.0;
   @override
+
+  TextEditingController firstController = TextEditingController();
+  TextEditingController secondController = TextEditingController();
+
   Widget build(BuildContext context) {
+    TextStyle? textStyle = Theme.of(context).textTheme.subtitle2;
     return Scaffold(
       appBar: AppBar(
         title: Text('Profile'),
       ),
-      body: Center(child: Text('Profile Data'))
+      body: Form(
+        child: Padding(
+          padding: EdgeInsets.all(padding*2),
+          child: ListView(
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.only(top: padding, bottom: padding),
+              child: TextFormField(
+                style: textStyle,
+                controller: firstController,
+                // validator: (String value){
+                //   if(value.isEmpty){
+                //     return 'Please enter your name';
+                //   }
+                // },
+                decoration: InputDecoration(
+                  labelText: 'First Name',
+                  hintText: 'Enter your name',
+                  labelStyle: textStyle,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0)
+                    )
+                )
+              )
+              ),
+              Padding(
+                  padding: EdgeInsets.only(top: padding, bottom: padding),
+                  child: TextFormField(
+                      style: textStyle,
+                      controller: secondController,
+                      // validator: (String value){
+                      //   if(value.isEmpty){
+                      //     return 'Please enter your name';
+                      //   }
+                      // },
+                      decoration: InputDecoration(
+                          labelText: 'Last Name',
+                          hintText: 'Enter your name',
+                          labelStyle: textStyle,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0)
+                          )
+                      )
+                  )
+              ),
+              ElevatedButton(onPressed: submit, child: Text("Submit"),),
+            ]
+          )
+        )
+      ),
     );
   }
 
