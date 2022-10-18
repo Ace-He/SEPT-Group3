29f47cbcbeafae8c23212c895748e5804ba45497
Jiaqi He s3831774 <s3831774@student.rmit.edu.au>
2022-10-18 11:43:50 +1100

    build jar, and fix bugs

--- Back-End/GpMicroService/src/test/java/com/Group3/GpControllerTest.java
+++ Back-End/GpMicroService/src/test/java/com/Group3/GpControllerTest.java
@@ -22,8 +22,8 @@ public class GpControllerTest {
 
     @Test
     public void listGp_fail() throws Exception{   // needs jwt token
-        mockMvc.perform(MockMvcRequestBuilders.get("/gp/listGp")).
-                andExpect(MockMvcResultMatchers.status().is(400)).
+        mockMvc.perform(MockMvcRequestBuilders.get("/listGp")).
+                andExpect(MockMvcResultMatchers.status().is(404)).
                 andDo(MockMvcResultHandlers.print()).andReturn();
     }
 
